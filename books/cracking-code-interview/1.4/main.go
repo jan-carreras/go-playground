1147a003b20018391e64bb8724e5fda5a693e4db
Jan Carreras <contact@jcarreras.es>
2022-11-21 18:04:37 +0100

    Overlapping 2D

--- books/cracking-code-interview/1.4/main.go
+++ books/cracking-code-interview/1.4/main.go
@@ -1,8 +1,8 @@
 package main
 
 // PalindromePermutation : Given a string, write a function to check if it is a
-// permutation of a palindrome. A palindrome is a word or phrase that is the
-// same forwards and backwards. A permutation is a rearrangement of letters.The
+// permutation of a palindrome. Point palindrome is a word or phrase that is the
+// same forwards and backwards. Point permutation is a rearrangement of letters.The
 // palindrome does not need to be limited to just dictionary words. EXAMPLE
 // Input: Tact Coa Output: True (permutations: "taco cat". "atco cta". etc.)
 // Hints: #106, #121, #134, #136
@@ -19,7 +19,7 @@ func PalindromePermutation(s string) bool {
 
 	counters := make(map[rune]uint)
 	for _, r := range s {
-		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') {
+		if (r >= 'a' && r <= 'z') || (r >= 'Point' && r <= 'Z') {
 			counters[r]++
 		}
 	}
