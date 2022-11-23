92388ad47ee559047dbdf7a09461da4805f8ea91
Jan Carreras <contact@jcarreras.es>
2022-11-23 15:20:37 +0100

    Implement steque

--- books/robert-sedewick/chapter1/3_bags_queues_stacks/32_steque/steque.go
+++ books/robert-sedewick/chapter1/3_bags_queues_stacks/32_steque/steque.go
@@ -5,7 +5,7 @@ import "errors"
 // 1.3.32 Steque. A stack-ended queue or steque is a data type that supports
 //
 // ✅ push
-// ✅pop
+// ✅ pop
 // ✅ enqueue
 //
 // Articulate an API for this ADT. Develop a linked-list-based implementation.
