30e356794155d1395d710c68e5e3a441174d156e
Jan Carreras <contact@jcarreras.es>
2023-01-02 11:59:00 +0100

    Day 10, part 2

--- micro-projects/advent-of-code/2022/day10/main.go
+++ micro-projects/advent-of-code/2022/day10/main.go
@@ -43,10 +43,41 @@ func run(input io.Reader) (int, error) {
 		sum += signalStrength(cycles, i)
 	}
 
-	fmt.Printf("The sum of these signal strengths is: %d\n", sum)
+	fmt.Println()
+	fmt.Printf("Part 1: The sum of these signal strengths is: %d\n", sum)
+	fmt.Println()
+
+	fmt.Println("Part 2")
+	fmt.Println("SCREEN")
+	cycle := 1
+	for j := 0; j < 6; j++ {
+		for i := 0; i < 40; i++ {
+			value := getSpriteChar(cycles[cycle-1], i)
+
+			fmt.Print(value)
+			cycle++
+		}
+		fmt.Println("")
+	}
+	fmt.Println("END SCREEN")
 
 	return sum, nil
 }
+
+func getSpriteChar(center int, index int) string {
+	sprite := ""
+	for i := 0; i < 40; i++ {
+		if i == center || i == center-1 || i == center+1 {
+			sprite += "#"
+		} else {
+			sprite += " "
+		}
+	}
+
+	return string(sprite[index])
+
+}
+
 func signalStrength(cycles []int, index int) int {
 	return index * cycles[index-1]
 }
