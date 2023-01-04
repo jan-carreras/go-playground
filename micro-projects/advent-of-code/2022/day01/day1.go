b296339bb5571daef6492e0b75d0333d54d464ff
Jan Carreras <contact@jcarreras.es>
2023-01-04 11:47:26 +0100

    Tidy up day1 code

--- micro-projects/advent-of-code/2022/day01/day1.go
+++ micro-projects/advent-of-code/2022/day01/day1.go
@@ -1,76 +1,85 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"io"
-	"log"
-	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
-func main() {
-	maxCalories, err := part1(os.Stdin)
+func part1(input io.Reader) (int, error) {
+	elfsCalories, err := readElfCalories(input)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
-	fmt.Printf("The elf with most calories is: %d\n", maxCalories)
-}
-
-func part1(input io.Reader) (maxCalories int, err error) {
-	s := bufio.NewScanner(input)
-
-	lastElfCalories := 0
-	for s.Scan() {
-		line := s.Text()
-		if line == "" {
-			maxCalories = max(maxCalories, lastElfCalories)
-			lastElfCalories = 0
-			continue
-		}
-
-		calories, err := strconv.Atoi(line)
-		if err != nil {
-			return 0, fmt.Errorf("invalid number: %w", err)
-		}
-
-		lastElfCalories += calories
-	}
+	maxCalories := maxInt(elfsCalories)
 
 	return maxCalories, nil
 }
 
 func part2(input io.Reader) (calories int, err error) {
-	s := bufio.NewScanner(input)
-
-	elfs := make([]int, 1)
-	for s.Scan() {
-		line := s.Text()
-		if line == "" {
-			elfs = append(elfs, 0)
-			continue
-		}
-
-		calories, err := strconv.Atoi(line)
-		if err != nil {
-			return 0, fmt.Errorf("invalid number: %w", err)
-		}
-
-		elfs[len(elfs)-1] += calories
+	elfs, err := readElfCalories(input)
+	if err != nil {
+		return 0, err
 	}
 
 	sort.Ints(elfs)
 
+	// Find the top three Elves carrying the most Calories
 	return sum(elfs[len(elfs)-3:]...), nil
+}
+
+// readElfCalories returns the calories being carried by each elf
+func readElfCalories(input io.Reader) (elfsCalories []int, err error) {
+	raw, err := io.ReadAll(input)
+	if err != nil {
+		return nil, err
+	}
+
+	// Each elf is differentiated to the next elf by a double \n
+	elfs := strings.Split(string(raw), "\n\n")
+	for _, elf := range elfs {
+		calories, err := parseInts(strings.Split(elf, "\n"))
+		if err != nil {
+			return nil, err
+		}
+		elfsCalories = append(elfsCalories, sum(calories...))
+	}
+
+	return elfsCalories, nil
+}
+
+func parseInts(strings []string) (rsp []int, err error) {
+	for _, s := range strings {
+		if s == "" { // Ignore empty lines. Might be present at the end of the input
+			continue
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, err
+		}
+		rsp = append(rsp, i)
+	}
+
+	return rsp, nil
 }
 
 func sum(input ...int) (r int) {
 	for _, i := range input {
 		r += i
 	}
+
 	return r
+}
+
+func maxInt(ints []int) int {
+	m := 0
+	for _, i := range ints {
+		m = max(m, i)
+	}
+
+	return m
 }
 
 func max(a, b int) int {
